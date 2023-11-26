c1aff4f2266a447c9dbc86ad2d01ed0d565fb68e
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-26 14:23:31 +0100

    E01_variable

---
+++ .idea/exercises/E01_variables.py
@@ -0,0 +1,57 @@
+wallet = 41
+print(wallet)
+
+money = 45.6
+print(money)
+
+state = "New York"
+print(state)
+
+wallet =32
+print("Wallet value is changed:", wallet)
+
+day = 21  #int
+temp = -15
+weight = 190.4  #float
+age =32
+bitcoin = 0.00345
+
+print(3+6)
+print(day+3)
+print(weight*2)
+print(weight/2)
+
+
+#String variables
+shirt = "blue"
+home = 'white'
+print(shirt)
+print(home)
+
+#here it is easier to use ""
+store = "Nick's Pizza Shop"
+print(store)
+
+#or
+store = 'Nick\'s Pizza Shop'
+print(store)
+
+store = "Nick's Pizza Shop, the \"best\" there is"
+print(store)
+
+movie = 'The Sandlot'
+print(movie)
+
+
+
+
+#boolean
+light_is_on = True
+
+if light_is_on:
+    print("The light is on")
+
+baby_is_asleep = False
+
+
+
