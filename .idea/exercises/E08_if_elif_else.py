e51b61c4e7eff2c592a4b4a74d2a308e778e99aa
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-29 22:44:13 +0100

    E08

---
+++ .idea/exercises/E08_if_elif_else.py
@@ -0,0 +1,55 @@
+light_is_on = False
+
+if light_is_on:
+    print("The light is on")
+else:
+    print("We are in the dark")
+
+
+
+#check weight
+weight =190.4
+
+if weight < 195:
+    print("You are under weight :) ")
+
+
+
+#check the person is adult or not
+age = 32
+if age>= 18:
+    print("Adult")
+else:
+    print("Child")
+
+
+
+#Check if the given number is odd or even
+number=int(input("Enter a number please: "))
+
+if number%2 == 0:
+    print("The number is even")
+else :
+    print("The number is odd")
+
+
+
+# if-elif-else
+if x>y:
+    print("x is greater than y")
+elif x==y:
+    print("x is equal to y")
+else:
+    print("x is less than y")
+print("---------------------------------------------------------------------")
+
+
+
+# if-else
+streng_value="10"
+heltall_verdi=5
+
+if int(streng_value)<heltall_verdi:
+    print("a")
+else:
+    print("b")
