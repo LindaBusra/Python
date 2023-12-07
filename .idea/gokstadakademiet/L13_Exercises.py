d6b88b807b321e0bad48fe55b340fad6fea38e01
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-12-07 20:16:29 +0100

    L13

---
+++ .idea/gokstadakademiet/L13_Exercises.py
@@ -0,0 +1,59 @@
+#Oppgave 1: Slett en fin kalt "slett_meg.txt" fra filsystemer
+import os
+
+# filnavn = "slett_meg.txt"
+# if os.path.exists(filnavn):
+#     os.remove(filnavn)
+#     print(f"{filnavn} er slettet")
+# else:
+#     print(f"{filnavn} eksisterer ikke")
+
+
+
+#Oppgave 2: Opprett en ny mappe kalt "ny_mappe" på filsystemet
+# mappe_navn="ny_mappe"
+#
+# if not os.path.exists(mappe_navn):
+#     os.mkdir(mappe_navn)
+#     print(f"{mappe_navn} er opprettet")
+# else:
+#     print(f"{mappe_navn} eksisterer allerede")
+
+
+
+#Oppgave 3: Sjekk om en fil med navn "sjekk_meg.txt" eksisterer på filsystemet
+
+# filnavn = "sjekk_meg.txt"
+#
+# if os.path.exists(filnavn):
+#     print(f"{filnavn} eksisterer")
+# else:
+#     print(f"{filnavn} eksisterer ikke")
+
+
+
+#Oppgave 4: List opp alle filer og mapper i gjeldende arbeidsmappe
+# innholds_liste = os.listdir()
+# for element in innholds_liste:
+#     print(element)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
