5007d58188a06b580834b8fab66c4caab47c9447
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-12-13 20:52:10 +0100

    C02

---
+++ .idea/w3schools.exercises/C02_Variables.py
@@ -0,0 +1,42 @@
+x=5
+y="Mike"    #Same as 'Mike'
+
+print(x)
+print(y)
+
+
+
+#Casting
+
+x=str(3)    #x will be '3'
+y=int(3)    #y will be 3
+z=float(3)  #z will be 3.0
+
+
+x = 5
+y = "John"
+z=3.0
+d=True
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(d))
+
+
+#float
+y = 12E4
+z = -87.7e100
+print(y)
+print(z)
+
+
+#complex
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(x+y+z)
