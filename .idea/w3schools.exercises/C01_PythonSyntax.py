c2bbf71d767c8e18a885355ab6ac5e04e07dab57
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-12-13 20:51:50 +0100

    C01

---
+++ .idea/w3schools.exercises/C01_PythonSyntax.py
@@ -0,0 +1,16 @@
+
+if 5>2:
+    print("5 is greater than two")
+
+#Python Comments
+#This a comment line, and we use # for multiline comments too.
+
+#Python will ignore string if it is not assigned to a variablle, so you can use string with triple quotes
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
