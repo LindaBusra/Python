9c42892edb5e4e996f6a0f2fd40b7a3d47d5d0dc
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-01-09 22:51:07 +0100

    C12_EscapeCharacter

---
+++ .idea/w3schools.exercises/C12_EscapeCharacter.py
@@ -0,0 +1,21 @@
+#Using Escape Character
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+txt = 'It\'s alright.'
+print(txt)
+
+txt = "This will insert one \\ (backslash)."
+print(txt)
+
+txt = "Hello\nWorld!"
+print(txt)
+
+txt = "Hello\tWorld!"
+print(txt)
+
+#This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt)
+
+
