dd43f40bb3c79f4a792b48fdce875b3d9bb83776
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-01-17 22:50:44 +0100

    C28_Collections

---
+++ .idea/w3schools.exercises/C28_Collections_nots.py
@@ -0,0 +1,15 @@
+# Python Collections (Arrays)
+# There are four collection data types in the Python programming language:
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+
+# Set is a collection which is unordered, unchangeable and unindexed.
+# Set items are unchangeable, but you can remove and/or add items whenever you like.No duplicate members.
+
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+# (As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.)
+
+
+#Resource : https://www.w3schools.com/python/python_tuples.asp
