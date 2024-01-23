4db5bf27401ea2daa3b84521bd7d888652abf9a2
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-01-23 18:55:51 +0100

    C41_mydmodule

---
+++ .idea/w3schools.exercises/C41_mymodule.py
@@ -0,0 +1,12 @@
+#The module named mymodule has one function and one dictionary:
+
+def greeting(name):
+    print("Hello, " + name)
+
+
+person1 = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
+
