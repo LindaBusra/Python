566b3d12087c9a63f82a1acd2306ed7daf607bed
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-08-08 21:41:45 +0200

    last

---
+++ .idea/w3schools/Basic.py
@@ -0,0 +1,29 @@
+a =5
+b=6
+print(a+b)
+
+name = 'Heidi'
+print(f"Hello {name}")
+
+#check The Python version of editor
+import sys
+print(sys.version)
+
+#indentation, (the number of spaces has to be et least one)
+if 5>3:
+    print("5 is greater than 3")
+else:
+    print("5 is not greater than 3")    
+    
+    
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
